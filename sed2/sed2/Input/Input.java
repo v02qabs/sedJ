ad61a6089bb0b83da14661cd2e507b26d63fd599
v02qabs <takesue090@gmail.com>
2024-10-19 14:24:35 +0900

    2024 10 19 14:24

---
+++ sed2/sed2/Input/Input.java
@@ -0,0 +1,12 @@
+package sed2.Input;
+
+import java.util.Scanner;
+
+public class Input{
+	public String Input(){
+		Scanner scan_data = new Scanner(System.in);
+		String data = scan_data.next();
+		return data;
+	}
+}
+
---
+++ sed2/sed2/sed2.java
@@ -0,0 +1,23 @@
+package sed2;
+
+import sed2.Input.Input;
+
+class sed2{
+	public static void main(String[] args){
+		System.out.println("ようこそsed2へ。");
+		new sed2();
+	}
+	public void sed2(){
+		System.out.println("編集前のファイル名：");
+		Input input_input_file = new Input();
+		String input_file_name = input_input_file.Input();
+		System.out.println("編集後のファイル名");
+		Input output_file = new Input();
+		String out_put_file_name = output_file.Input();
+		System.out.println("編集前の単語");
+		Input before_words = new Input();
+	}
+
+
+}
+
