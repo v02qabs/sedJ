214219aad078cceebc42f8a71d2c5e5dab5dcc11
v0qabs <takesue090@gmail.com>
2025-02-20 07:32:01 +0000

    2025-02-20 pm 2:00

--- sed/sed/sed.java
+++ sed/sed/sed.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 import hiro.*;
 
@@ -16,15 +17,18 @@ public class sed {
 	}
 	private void init() {
 			System.out.println("Start the sed command.");
-			//編集前の開くファイル
-			System.out.print("編集前の開くファイル:");
-			String string_before_file_name = new InputJ().InputJ();
-			System.out.print("編集後の保存先ファイル：");
-			String string_after_file_name = new InputJ().InputJ();
-			System.out.print("編集前の単語");
-			String string_before = new InputJ().InputJ();
-			System.out.print("編集後の単語");
-			String string_after = new InputJ().InputJ();
+			System.out.print("before file name : ");
+			Scanner scan_before_file_name = new Scanner(System.in);
+			String string_before_file_name = scan_before_file_name.next();
+			System.out.print("send files : ");
+			Scanner scan_after_file_name = new Scanner(System.in);
+			String string_after_file_name = scan_after_file_name.next();
+			System.out.print("fix before : ");
+			Scanner scan_before = new Scanner(System.in);
+			String string_before = scan_before.next();
+			System.out.print("fix after : ");
+			Scanner scan_after = new Scanner(System.in);
+			String string_after = scan_after.next();
 			new sed().sed(string_before_file_name, string_after_file_name, string_before, string_after);
 			
 	}
@@ -45,7 +49,7 @@ public class sed {
 	private void fixed(String fixed, String string_after_file_name) {
 		// TODO Auto-generated method stub
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(string_after_file_name)));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(string_after_file_name),true));
 			bw.write(fixed);
 			bw.close();
 		}catch(Exception error) {
